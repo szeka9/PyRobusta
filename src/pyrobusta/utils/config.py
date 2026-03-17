73c7da869b1dee0f619e45c762bf8cf66b30d73a
szeka9 <szkalman79@gmail.com>
2026-03-18 00:19:33 +0100

    Allow default values in pyrobusta config
    Use default values so that user overrides
    are not mandatory. This also helps running
    functional tests without the need for
    additional config loaded in the test environment.

--- src/pyrobusta/utils/config.py
+++ src/pyrobusta/utils/config.py
@@ -4,6 +4,7 @@ configuration is read from /pyrobusta.env.
 Values can be encapsulated by single or double quotes.
 """
 
+CONFIG_LOADED = False
 CONFIG_LOCATION = "pyrobusta.env"
 CONFIG_CACHE = [
     "wifi_ssid",
@@ -11,11 +12,11 @@ CONFIG_CACHE = [
     "wifi_password",
     None,
     "http_multipart",
-    None,
+    "False",
     "http_mem_cap",
-    None,
+    0.1,
     "socket_max_con",
-    None,
+    2,
 ]
 
 
@@ -24,19 +25,22 @@ def read_config(config=CONFIG_LOCATION):
     Read configuration from a file and update CONFIG_CACHE.
     :param config: path to configuration
     """
-    with open(config, encoding="utf-8") as conf:
-        for line in conf.read().splitlines("\n"):
-            key = line.split("=")[0].strip()
-            value = line.split("=")[1].strip().strip("'").strip('"')
-            if key and value:
-                if (
-                    key in CONFIG_CACHE
-                    and (conf_idx := CONFIG_CACHE.index(key)) % 2 == 0
-                ):
-                    CONFIG_CACHE[conf_idx + 1] = value
-                else:
-                    CONFIG_CACHE.append(key)
-                    CONFIG_CACHE.append(value)
+    try:
+        with open(config, encoding="utf-8") as conf:
+            for line in conf.read().splitlines("\n"):
+                key = line.split("=")[0].strip()
+                value = line.split("=")[1].strip().strip("'").strip('"')
+                if key and value:
+                    if (
+                        key in CONFIG_CACHE
+                        and (conf_idx := CONFIG_CACHE.index(key)) % 2 == 0
+                    ):
+                        CONFIG_CACHE[conf_idx + 1] = value
+                    else:
+                        CONFIG_CACHE.append(key)
+                        CONFIG_CACHE.append(value)
+    except OSError:
+        pass
 
 
 def get_config(key):
@@ -45,8 +49,10 @@ def get_config(key):
     The cache is reloaded if the key is missing
     or the value is set to None.
     """
-    if key not in CONFIG_CACHE:
+    global CONFIG_LOADED  # pylint: disable=W0603
+    if key not in CONFIG_CACHE or not CONFIG_LOADED:
         read_config()
+        CONFIG_LOADED = True
     try:
         conf_idx = CONFIG_CACHE.index(key)
     except IndexError:
